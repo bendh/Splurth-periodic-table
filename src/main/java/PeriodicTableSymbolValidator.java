db3bad0e7be975a979c10523ebb612df9ffe6474
Ben Ooms <ben.ooms@ing.nl>
2016-08-11 12:33:47 +0200

    Added validation

--- src/main/java/PeriodicTableSymbolValidator.java
+++
@@ -1,7 +0,0 @@
-package PACKAGE_NAME;
-
-/**
- * Created by Ben Ooms on 11-8-2016.
- */
-public class PeriodicTableSymbolValidator {
-}
--- src/test/groovy/nl/benooms/splurthperiodictableSpec.groovy
+++ src/test/groovy/nl/benooms/splurthperiodictableSpec.groovy
@@ -1,6 +1,8 @@
 package nl.benooms
 
+import nl.benooms.splurthperiodictable.PeriodicTableSymbolValidator
 import spock.lang.Specification
+import spock.lang.Unroll
 
 
 /**
@@ -12,6 +14,26 @@ class splurthperiodictableSpec extends Specification {
     def "Dummy test to check setup" () {
         expect:
             assert true
+    }
+
+    @Unroll
+    def "Given a symbol: #symbol and a element: #element a validation result: #expectedResult is returned" (symbol, element, expectedResult) {
+        when:
+            def result = new PeriodicTableSymbolValidator().validateSymbol(symbol, element)
+        then:
+            assert result == expectedResult
+        where:
+            symbol  |   element           |   expectedResult
+            "t"     |   "ether"           |   false //Length of symbol must be 2
+            "123"   |   "ether"           |   false //Length of symbol must be 2
+            ""      |   "ether"           |   false //Length of symbol must be 2
+             null   |   "ether"           |   false //Length of symbol must be 2
+            "az"    |   ""                |   false //Length of element must be 2 or higher
+            "AZ"    |   "e"               |   false //Length of element must be 2 or higher
+            "ZN"    |   null              |   false //Length of element must be 2  or higher
+            "12"    |   "abd"             |   false // only a-z A-Z allowed
+            "AB"    |   "YZ"              |   true
+            "AB"    |   "YZA"             |   true
     }
 
 }
