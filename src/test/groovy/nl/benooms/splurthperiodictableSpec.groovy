d33680e2a6ae6ead31cdd1909d6d3e1ede54d9f5
Ben Ooms <ben.ooms@ing.nl>
2016-08-11 09:52:13 +0200

    Initial commit with spock setup

---
+++ src/test/groovy/nl/benooms/splurthperiodictableSpec.groovy
@@ -0,0 +1,17 @@
+package nl.benooms
+
+import spock.lang.Specification
+
+
+/**
+ * Created by Ben Ooms on 11-8-2016.
+ *
+ */
+class splurthperiodictableSpec extends Specification {
+
+    def "Dummy test to check setup" () {
+        expect:
+            assert true
+    }
+
+}
